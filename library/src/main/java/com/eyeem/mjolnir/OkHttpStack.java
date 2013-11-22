b775a4f700ecb1678738616eef1c76377eb45a75
Lukasz Wisniewski <luke.cherrish@gmail.com>
2013-11-22 16:02:21 +0100

    SSL fix for the okhttpstack

--- library/src/main/java/com/eyeem/mjolnir/OkHttpStack.java
+++ library/src/main/java/com/eyeem/mjolnir/OkHttpStack.java
@@ -5,6 +5,9 @@ import com.squareup.okhttp.OkHttpClient;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+
+import javax.net.ssl.SSLContext;
 
 /**
  * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which
@@ -22,6 +25,19 @@ public class OkHttpStack extends HurlStack {
          throw new NullPointerException("Client must not be null.");
       }
       this.client = client;
+   }
+
+   public static OkHttpStack withSslWorkaround() {
+      OkHttpClient client = new OkHttpClient();
+      SSLContext sslContext; // workaround for okhttp issue #184, should be fixed in 2.0
+      try {
+         sslContext = SSLContext.getInstance("TLS");
+         sslContext.init(null, null, null);
+      } catch (GeneralSecurityException e) {
+         throw new AssertionError(); // The system has no TLS. Just give up.
+      }
+      client.setSslSocketFactory(sslContext.getSocketFactory());
+      return new OkHttpStack(client);
    }
 
    @Override protected HttpURLConnection createConnection(URL url) throws IOException {
